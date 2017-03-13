1f37c1b1d5cab91e5857b880b9b6ed281dccd42b
Chris Stanton <cstanton0760@yahoo.com>
2017-03-13 14:40:22 -0500

    edited hero-edit view/was broken now working

--- server/public/controllers/hero.edit.controller.js
+++ server/public/controllers/hero.edit.controller.js
@@ -1,27 +1,24 @@
 app.controller("HeroEditController", ["$http", function($http) {
-  console.log("HeroEditController is working");
+  console.log("HeroController is running");
 
   var self = this;
-//container
   self.heroes = [];
-//calls getHeroes()
+
   getHeroes();
 
   function getHeroes() {
     $http.get('/heroes')
       .then(function(response) {
-        //console.log(response);
-        //console.log('heroes: ', self.heroes);
+        console.log(response);
         self.heroes = response.data;
-      })//end  of .then
+      })//end of .then
   }//end of getHeroes()
 
   self.deleteHero = function(hero) {
     $http.delete('/heroes/' + hero.id)
       .then(function(response) {
-        //console.log("deleted hero: ", hero.name );
+        console.log("deleted hero: ", hero.name );
         getHeroes();
       })//end of .then
-  }//end of deleteHero()
-
+  }//end of self.deleteHero()
 }]);//end controller
