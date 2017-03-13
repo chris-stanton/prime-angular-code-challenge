064680373a2c98174a488e788a6f418310d5f634
Chris Stanton <cstanton0760@yahoo.com>
2017-03-13 13:22:22 -0500

    updated hero-list.js

--- server/public/scripts/controllers/hero-list.js
+++ server/public/scripts/controllers/hero-list.js
@@ -1,3 +1,26 @@
-app.controller('HeroListController', function(){
-    console.log('Hero List Controller loaded');
-});
+app.controller("HeroEntryController", ["$http", function($http) {
+  console.log("HERO ENTRY CONTROLLER IS RUNNING!");
+
+  var self = this;
+  self.newHero = {};
+  self.powers = [];
+
+//calling get powers function
+  getPowers();
+
+  self.addHero = function() {
+    $http.post('/heroes', self.newHero)
+      .then(function(response) {
+        console.log("New hero added to database!");
+        self.newHero = {};
+      })//end of .then
+  }//end of self.addHero
+
+  function getPowers() {
+    $http.get('/powers')
+      .then(function(response) {
+        self.powers = response.data;
+        console.log("Got powers from database");
+      })//end of .end then
+  }//end of getPowers()
+}]);//end controller
