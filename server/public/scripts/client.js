8270dadf396bf5294b217e8476416750d1e65103
Chris Stanton <cstanton0760@yahoo.com>
2017-03-13 13:56:07 -0500

    updated superpowers.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,17 +2,19 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
+//view to add super_heroes to database
     .when('/heroentry', {
       templateUrl: '/views/templates/hero-entry.html',
       controller: 'HeroEntryController',
       controllerAs: 'entry'
     })
+//view to delete super_heroes from lista and database
     .when('/heroedit', {
       templateUrl: '/views/templates/hero-edit.html',
       controller: 'HeroEditController',
       controllerAs: 'edit'
     })
     .otherwise({
-      redirectTo: 'heroedit'
+      redirectTo: 'heroentry'
     })
 }]);
--- server/routes/superpowers.js
+++ server/routes/superpowers.js
@@ -9,19 +9,19 @@ var pool = new pg.Pool({
 
 // return all super power names
 router.get('/', function (req, res) {
-  console.log('getting powers');
   pool.connect()
     .then(function (client) {
+//selects all super_powers in ASC order
       client.query('SELECT * FROM super_powers ORDER BY name ASC')
         .then(function (result) {
           client.release();
           res.send(result.rows);
         })
         .catch(function (err) {
-          console.log('error on SELECT', err);
+          console.log('error on get super_power names', err);
           res.sendStatus(500);
         });
-    });    
+    });
 });
 
 
